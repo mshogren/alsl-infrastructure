c49dc9ebd0ecd85f658158add939f90b60b095af
mshogren <3902009+mshogren@users.noreply.github.com>
2016-02-01 14:30:25 -0700

    Fix script to attache policy

--- aws/setup.sh
+++ aws/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+userArn=$(aws iam get-user --query User.Arn);
+baseArn=${userArn:1:26};
+
 aws iam create-user --user-name alsl-ec2-launch
 aws iam create-policy --policy-name alsl-ec2-launch --policy-document file://alsl-ec2-launch-policy.json
-aws iam attach-user-policy --user-name alsl-ec2-launch --policy-arn 
+aws iam attach-user-policy --user-name alsl-ec2-launch --policy-arn ${baseArn}policy/alsl-ec2-launch
