cf1d0a9c40bc6d299b2cc085cce7885a2715e9b3
mshogren <3902009+mshogren@users.noreply.github.com>
2016-02-01 14:44:06 -0700

    Fix setup script to recreate policy each run

--- aws/setup.sh
+++ aws/setup.sh
@@ -4,5 +4,7 @@ userArn=$(aws iam get-user --query User.Arn);
 baseArn=${userArn:1:26};
 
 aws iam create-user --user-name alsl-ec2-launch
+aws iam detach-user-policy --user-name alsl-ec2-launch --policy-arn ${baseArn}policy/alsl-ec2-launch
+aws iam delete-policy --policy-arn ${baseArn}policy/alsl-ec2-launch
 aws iam create-policy --policy-name alsl-ec2-launch --policy-document file://alsl-ec2-launch-policy.json
 aws iam attach-user-policy --user-name alsl-ec2-launch --policy-arn ${baseArn}policy/alsl-ec2-launch
