9d4f089ab5192978f3e1f12dc3bada97d37b221e
mshogren <3902009+mshogren@users.noreply.github.com>
2017-12-06 08:25:18 -0700

    Script to request AWS EC2 spot instances

---
+++ aws/request-devenv-spot-instance.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+IP=$(curl http://checkip.amazonaws.com);
+
+aws ec2 describe-security-groups --group-names alsl-ec2-dev-sg
+
+EXISTS=$?;
+
+if [ $EXISTS -ne 0 ]; then
+  aws ec2 create-security-group --group-name alsl-ec2-dev-sg --description "Security group for developer instances";
+fi
+
+aws ec2 describe-security-groups --group-names alsl-ec2-dev-sg --query SecurityGroups[0].IpPermissions > tmp.json
+
+aws ec2 revoke-security-group-ingress --group-name alsl-ec2-dev-sg --ip-permissions file://tmp.json
+
+rm tmp.json
+
+aws ec2 authorize-security-group-ingress --group-name alsl-ec2-dev-sg --protocol tcp --port 22 --cidr $IP/32
+
+USERDATA=$(base64 userdata.sh -w 0)
+QUERY="Images[0].{ImageId: ImageId, InstanceType: \`\"m3.medium\"\`, KeyName: \`\"${USER}\"\`, SecurityGroups: [ \`\"alsl-ec2-dev-sg\"\` ], IamInstanceProfile: { Name: \`\"alsl-ec2-dev\"\` }, UserData: \`\"${USERDATA}\"\`, BlockDeviceMappings: BlockDeviceMappings[*] }"
+
+aws ec2 describe-images --image-id ami-790ec601 --query "$QUERY" > launch.json
+
+sed -i 's/8,/16,/' launch.json
+sed -i '/"Encrypted": false,/d' launch.json
+
+NEXTYEAR=$(date -d "next year" -Is)
+
+aws ec2 request-spot-instances --spot-price "0.012" --type "persistent" --valid-until "$NEXTYEAR" --instance-interruption-behavior "stop" --launch-specification file://launch.json
+
+rm launch.json
