2dc600d102c1188aa0860413a635dd813d91814a
mshogren <3902009+mshogren@users.noreply.github.com>
2018-08-16 14:01:24 +0000

    Add tests for awscli role

---
+++ ansible/roles/awscli/molecule/default/tests/test_default.py
@@ -0,0 +1,11 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    region = host.check_output('aws configure get region')
+    assert region == 'region-1'
