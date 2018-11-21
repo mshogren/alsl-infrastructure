fc5bfdc41738d255ced4ba3e69e8404fb9c98394
mshogren <3902009+mshogren@users.noreply.github.com>
2018-11-21 15:39:42 -0700

    update AWS EC2 image ids

--- aws/launch-devenv.sh
+++ aws/launch-devenv.sh
@@ -12,12 +12,12 @@ rm tmp.json
 
 aws ec2 authorize-security-group-ingress --group-name alsl-ec2-dev-sg --protocol tcp --port 22 --cidr $IP/32
 
-aws ec2 describe-images --image-id ami-0f47ef92b4218ec09 --query 'Images[0].BlockDeviceMappings' > mapping.json
+aws ec2 describe-images --image-id ami-079b4e9085609225c --query 'Images[0].BlockDeviceMappings' > mapping.json
 
 sed -i 's/8,/16,/' mapping.json
 sed -i '/"Encrypted": false/d' mapping.json
 sed -i 's/"gp2",/"gp2"/' mapping.json
 
-aws ec2 run-instances --image-id ami-0f47ef92b4218ec09 --key-name $USER --security-groups alsl-ec2-dev-sg --instance-type t2.micro --block-device-mappings file://mapping.json --iam-instance-profile Name=alsl-ec2-dev --user-data file://userdata.sh
+aws ec2 run-instances --image-id ami-079b4e9085609225c --key-name $USER --security-groups alsl-ec2-dev-sg --instance-type t2.micro --block-device-mappings file://mapping.json --iam-instance-profile Name=alsl-ec2-dev --user-data file://userdata.sh
 
 rm mapping.json
