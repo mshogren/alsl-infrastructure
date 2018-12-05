9df7454c3f2b4ef35450e95c0da72ce92af2b4ab
mshogren <3902009+mshogren@users.noreply.github.com>
2018-12-05 16:15:56 -0700

    Add role to set timezone

---
+++ ansible/roles/env/molecule/default/tests/test_default.py
@@ -0,0 +1,11 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    timeoffset = host.check_output('date +%z')
+    assert timeoffset == '+0500'
