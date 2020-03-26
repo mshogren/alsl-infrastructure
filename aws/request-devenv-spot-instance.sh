89d1758657193560b1b2b755a32a522cc3518150
mshogren <3902009+mshogren@users.noreply.github.com>
2020-03-26 13:10:06 -0600

    update AWS EC2 image ids

--- aws/request-devenv-spot-instance.sh
+++ aws/request-devenv-spot-instance.sh
@@ -21,7 +21,7 @@ aws ec2 authorize-security-group-ingress --group-name alsl-ec2-dev-sg --protocol
 USERDATA=$(base64 userdata.sh -w 0)
 QUERY="Images[0].{ImageId: ImageId, InstanceType: \`\"t3a.medium\"\`, Placement: { AvailabilityZone: \`\"us-west-2b\"\` }, KeyName: \`\"${USER}\"\`, SecurityGroups: [ \`\"alsl-ec2-dev-sg\"\` ], IamInstanceProfile: { Name: \`\"alsl-ec2-dev\"\` }, UserData: \`\"${USERDATA}\"\`, BlockDeviceMappings: BlockDeviceMappings[*] }"
 
-aws ec2 describe-images --image-id ami-0a7d051a1c4b54f65 --query "$QUERY" > launch.json
+aws ec2 describe-images --image-id ami-0e23ea41be77feacd --query "$QUERY" > launch.json
 
 sed -i 's/8,/16,/' launch.json
 sed -i '/"Encrypted": false,/d' launch.json
